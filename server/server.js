147dac331fa317ec987f96d2c272d1b954caa8f0
unknown <you@example.comcavidmelikli@gmail.com>
2019-07-09 20:32:47 +0400

    GiT/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');          //npm i express@4.14.0 --save
 const bodyParser = require('body-parser');   //npm i body-parser@1.15.2 --save
 const mongoose = require("./db/mongoose.js");
+const { ObjectId } = require('mongodb');
 
 const { Todo } = require('./moduls/todo.js');
 const { User } = require('./moduls/users.js');
@@ -21,16 +22,35 @@ app.post('/todos', (req, res) => {
     });
 });
 
-app.get('/todos', (req,res)=>{
-    Todo.find().then((todos)=>{
-        res.send({todos})                      //json oldugu ucun obyekt kimi yazdiq yeni {}-icinde
-    },(e)=>{
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos })                      //json oldugu ucun obyekt kimi yazdiq yeni {}-icinde
+    }, (e) => {
         res.status(400).send(e)
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id
+
+    if (!ObjectId.isValid(id)) {
+        res.status(404).send();
+    }
+
+    Todo.findById(id).then((todos) => {
+        if (!todos) {
+          return res.status(404).send();
+        };
+
+        res.send({ todos })
+    }).catch((e) => {
+        res.status(400).send();
+    })
+
 })
 
 app.listen(3000, () => {
     console.log('Started on port 3000')
 });
 
-module.exports={app};
+module.exports = { app };
