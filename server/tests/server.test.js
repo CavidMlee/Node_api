3217cb9fed87d9b322096148ec5d099675bbf11f
unknown <you@example.comcavidmelikli@gmail.com>
2019-07-16 00:09:35 +0400

    Work all test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -214,8 +214,8 @@ describe('POST /users', () => {                         //tokene gore test post 
                 if (err) {
                     return done(err);
                 }
-                
-                User.findOne({email}).then((user) => {
+
+                User.findOne({ email }).then((user) => {
                     expect(user).toExist();
                     expect(user.password).toNotBe(password);
                     done();
@@ -249,53 +249,72 @@ describe('POST /users', () => {                         //tokene gore test post 
 //Post/user/login testi
 describe('POST /users/login', () => {
     it('should login user and return auth token', (done) => {
-      request(app)
-        .post('/users/login')
-        .send({
-          email: users[1].email,
-          password: users[1].password
-        })
-        .expect(200)
-        .expect((res) => {
-          expect(res.headers['x-auth']).toExist();
-        })
-        .end((err, res) => {
-          if (err) {
-            return done(err);
-          }
-  
-          User.findById(users[1]._id).then((user) => {
-            expect(user.tokens[0]).toInclude({
-              access: 'auth',
-              token: res.headers['x-auth']
-            });
-            done();
-          }).catch((e) => done(e));
-        });
-    });
-  
-    it('should reject invalid login', (done) => {
-      request(app)
-        .post('/users/login')
-        .send({
-          email: users[1].email,
-          password: users[1].password + '1'
-        })
-        .expect(400)
-        .expect((res) => {
-          expect(res.headers['x-auth']).toNotExist();
-        })
-        .end((err, res) => {
-          if (err) {
-            return done(err);
-          }
-  
-          User.findById(users[1]._id).then((user) => {
-            expect(user.tokens.length).toBe(0);
-            done();
-          }).catch((e) => done(e));
-        });
-    });
-  });
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toExist();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
 
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens[0]).toInclude({
+                        access: 'auth',
+                        token: res.headers['x-auth']
+                    });
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should reject invalid login', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password + '1'
+            })
+            .expect(400)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toNotExist();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+});
+
+// Log out isi tokeni silirik
+describe('DELETE uders/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e))
+            });
+    });
+})
 //npm run test-watch
